43f73a7ab0b021e1fa4546476fbe2d7fbc2d126e
Usman00111 <123650130+Usman00111@users.noreply.github.com>
2025-08-16 20:52:55 +0000

    generated onboarding page

--- unitask/src/app/app-routing.module.ts
+++ unitask/src/app/app-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
   {
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'onboarding',
+    loadChildren: () => import('./onboarding/onboarding.module').then( m => m.OnboardingPageModule)
   }
 ];
 
