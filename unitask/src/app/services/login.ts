63b100551cb7e745dcb16aa1c8c691041a921939
Usman00111 <123650130+Usman00111@users.noreply.github.com>
2025-08-16 19:26:02 +0000

    implemented login redirect with msal

--- unitask/src/app/services/login.ts
+++ unitask/src/app/services/login.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@angular/core';
+import { MsalService } from '@azure/msal-angular';
+import { RedirectRequest } from '@azure/msal-browser';
 
 @Injectable({
   providedIn: 'root'
 })
-export class Login {
+export class LoginService {
+
+  constructor(private msalService: MsalService) {}
+
+  login() {
+    const loginRequest: RedirectRequest = {
+      scopes: ['user.read'] //ms grapgh scope to read profilee
+    };
+    this.msalService.loginRedirect(loginRequest);
+  }
   
 }
