16c2ad8d3ec3969c38040386f7ed6f19257d61dc
Usman00111 <123650130+Usman00111@users.noreply.github.com>
2025-08-16 20:21:26 +0000

    conneted login page to login service and also fixed the error where the app wont run as it was throwing standalone error and for that i had specifically add standalone: false line to ts files

---
+++ unitask/src/app/login/login.page.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { LoginService } from '../services/login.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+  standalone: false
+})
+export class LoginPage implements OnInit {
+
+  //injecting custom loginservice
+  constructor(private loginService: LoginService) { }
+
+  //triggered when user taps the button in the html 
+  loginWithMicrosoft(){
+    //this calls service which performs the msal login redirect 
+    this.loginService.login();
+  }
+
+  ngOnInit() {
+  }
+
+}
