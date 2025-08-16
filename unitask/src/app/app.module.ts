9105d4f208458492f3a6a0d87ae29d70896103e4
Usman00111 <123650130+Usman00111@users.noreply.github.com>
2025-08-16 19:13:51 +0000

    feature athentication: added microsoft config for login

--- unitask/src/app/app.module.ts
+++ unitask/src/app/app.module.ts
@@ -7,9 +7,25 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import {MsalModule, MsalRedirectComponent, MsalService} from '@azure/msal-angular';
+import {PublicClientApplication, InteractionType} from '@azure/msal-browser';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, MsalModule.forRoot(new PublicClientApplication({ auth: {
+          clientId: 'YOUR_AZURE_APP_CLIENT_ID',
+          authority: 'https://login.microsoftonline.com/common',
+          redirectUri: 'http://localhost:8100'
+        }
+
+    }),{
+        interactionType: InteractionType.Redirect
+      },
+      {
+        interactionType: InteractionType.Redirect,
+        protectedResourceMap: new Map()
+    }
+  )],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
