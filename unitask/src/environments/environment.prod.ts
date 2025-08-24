565223ad811b2ae1c70352e5c7d9aad64abf56ae
Usman00111 <123650130+Usman00111@users.noreply.github.com>
2025-08-24 11:27:11 +0000

    added environment.prod.ts for Angular prod builds

---
+++ unitask/src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'my_link',
+    authDomain: 'my_link',
+    projectId: 'my_link',
+    storageBucket: 'my_link',
+    messagingSenderId: 'my_link',
+    appId: 'my_link',
+  },
+};
+
